8f904d4a0cc79a5a80f09607dbb979c22499735a
jasonw52603 <minecrafter10k@gmail.com>
2019-09-25 00:01:20 -0400

    added counter for random

--- messageEvents.py
+++ messageEvents.py
@@ -3,6 +3,9 @@ import random as rand
 
 client = dc.Client()
 
+boomerYesCount = 0
+boomerNoCount = 0
+
 @client.event
 async def handleMessage(message, messageContent, user, authorID):
     # if user == '10mbRAM':
@@ -18,13 +21,23 @@ async def handleMessage(message, messageContent, user, authorID):
     #     else:
     #         await message.channel.send('yes boomer')
 
+    global boomerNoCount
+    global boomerYesCount
+
     if messageContent == '?boomer':
         boomer = rand.randint(0,1)
         if boomer == 0:
             await message.channel.send('<@' + str(authorID) + '>' + ' is no boomer')
+            boomerNoCount += 1
+            print(boomerNoCount)
         elif boomer == 1:
             await message.channel.send('<@' + str(authorID) + '>' + ' is yes boomer')
+            boomerYesCount += 1
+            print(boomerYesCount)
 
+    if messageContent == '?boomercount':
+        await message.channel.send('yes: ' + str(boomerYesCount))
+        await message.channel.send('no: ' + str(boomerNoCount))
 
     print(messageContent)
     print(user)
