9688f9960541590a222afae2d12ad962a946804f
jasonw52603 <minecrafter10k@gmail.com>
2019-10-01 22:45:36 -0400

    basic boomer code is now invoked with commands

--- bot.py
+++ bot.py
@@ -1,30 +1,25 @@
 import discord as dc 
 import os
 from dotenv import load_dotenv
-# import messageEvents as mEvents
 from discord.ext.commands import Bot
 from discord.ext import commands
 # import eventCreator 
+import messageEvents as mEvents
 
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
-BOT_PREFIX = ('?')
+BOT_PREFIX = ('?') #Eventually put this into a config/.env file
 
 bot = commands.Bot(command_prefix=(BOT_PREFIX))
-client = dc.Client()
 
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    print(f'{bot.user} has connected to Discord!')
     
 
-dateEntered = False
-
 @bot.command()
-async def boomer(ctx, arg):
-    print(arg)
-    print(ctx)
-    await ctx.send(arg)
+async def boomer(ctx, *arg):
+    await mEvents.handleBoomer(ctx, *arg)
 
-client.run(token)
+bot.run(token)
--- messageEvents.py
+++ messageEvents.py
@@ -2,63 +2,14 @@ import discord as dc
 import random as rand
 import prepFuncts as fn
 
-client = dc.Client()
+async def handleBoomer(ctx, *arg):
+    if len(arg) == 0:
+        await ctx.send('you are boomer')
+    elif len(arg) > 0:
+        await ctx.send('they are boomer')
+        print(arg)
+        print(len(arg))
 
-boomerYesCount = 0
-boomerNoCount = 0
-
-@client.event
-async def handleMessage(message, messageContent, user, authorID, mentionedUser):
-    # if user == '10mbRAM':
-    #     await message.channel.send(user + ' hello')
-    # elif user == 'Whole Wheat Orange':
-    #     await message.channel.send(user + ' is boomer')
-    # else:
-    #     await message.channel.send('bruh moment')
-
-    # if messageContent == '?boomer':
-    #     if user == '10mbRAM':
-    #         await message.channel.send('jason is not boomer')
-    #     else:
-    #         await message.channel.send('yes boomer')
-
-    global boomerNoCount
-    global boomerYesCount
-
-    if messageContent.startswith('?boomer'):
-        boomer = rand.randint(0, 1)
-
-        if len(str(mentionedUser)) > 0:
-            if boomer == 0:
-                await message.channel.send('<@' + str(mentionedUser) + '>' + ' is no boomer')
-                boomerNoCount += 1
-                print(boomerNoCount)
-            elif boomer == 1:
-                await message.channel.send('<@' + str(mentionedUser) + '>' + ' is yes boomer')
-                boomerYesCount += 1
-                print(boomerYesCount)
-        
-        if messageContent == '?boomer':
-            if boomer == 0:
-                await message.channel.send('<@' + str(authorID) + '>' + ' is no boomer')
-                boomerNoCount += 1
-                print(boomerNoCount)
-            elif boomer == 1:
-                await message.channel.send('<@' + str(authorID) + '>' + ' is yes boomer')
-                boomerYesCount += 1
-                print(boomerYesCount)
-
-        if messageContent == '?boomercount':
-            await message.channel.send('yes: ' + str(boomerYesCount))
-            await message.channel.send('no: ' + str(boomerNoCount))
-
-    if messageContent in fn.boomerMisspellings:
-        await message.channel.send('learn to type, loser' + ' <:face:627141817678168064>')
-
-    print(messageContent)
-    print(user)
-    # await message.channel.send('@' + user + ' sent a message!')
-    # print('pinged message')
     
         
 
