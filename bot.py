816c7ed6d2f7d9ddce5d3fe5b5214491de6ad257
jasonw52603 <minecrafter10k@gmail.com>
2019-10-03 14:42:54 -0400

    full boomer functionality with ext.commands

--- bot.py
+++ bot.py
@@ -3,6 +3,8 @@ import os
 from dotenv import load_dotenv
 from discord.ext.commands import Bot
 from discord.ext import commands
+from discord.ext.commands import CommandNotFound
+import prepFuncts as fn
 # import eventCreator 
 import messageEvents as mEvents
 
@@ -17,10 +19,35 @@ bot = commands.Bot(command_prefix=(BOT_PREFIX))
 @bot.event
 async def on_ready():
     print(f'{bot.user} has connected to Discord!')
-    
 
+@bot.event
+async def on_command_error(ctx, error):
+    print('Bad command: ' + ctx.message.content)
+    if ctx.message.content in fn.boomerMisspellings:
+        await ctx.send('learn to type, boomer' + ' <:face:627141817678168064>')
+    else:
+        await ctx.send('learn to type, loser' + ' <:face:627141817678168064>')
+
+ 
+    
 @bot.command()
 async def boomer(ctx, *arg):
-    await mEvents.handleBoomer(ctx, *arg)
+    author = ctx.author.id
+    try:
+        mentionedUser = ctx.message.mentions[0].id
+    except:
+        mentionedUser = ''
+    await mEvents.handleBoomer(ctx, author, mentionedUser, *arg)
+    print(ctx.author.id)
+
+@bot.command()
+async def boomercount(ctx, arg='()'):
+    if arg != '()':
+        return
+    await mEvents.handleBoomercount(ctx)
+
+
+
+
 
 bot.run(token)
--- messageEvents.py
+++ messageEvents.py
@@ -2,14 +2,34 @@ import discord as dc
 import random as rand
 import prepFuncts as fn
 
-async def handleBoomer(ctx, *arg):
-    if len(arg) == 0:
-        await ctx.send('you are boomer')
-    elif len(arg) > 0:
-        await ctx.send('they are boomer')
+boomerNo = 0
+boomerYes = 0
+
+async def handleBoomer(ctx, author, mentionedUser, *arg):
+    global boomerNo
+    global boomerYes
+
+    bChance = rand.randint(0,1)
+    if len(arg) == 0 and bChance == False:
+        await ctx.send('you are not boomer, ' + '<@' + str(author) + '>')
+        boomerNo += 1
+
+    elif len(arg) == 0 and bChance == True:
+        await ctx.send('you are boomer, ' + '<@' + str(author) + '>')
+        boomerYes += 1
+
+    elif len(str(mentionedUser)) > 0 and bChance == 0:
+        await ctx.send('<@' + str(mentionedUser) + '>' + ' is not boomer')
+        boomerNo += 1
         print(arg)
         print(len(arg))
 
-    
+    elif len(str(mentionedUser)) > 0 and bChance == 1:
+        await ctx.send('<@' + str(mentionedUser) + '>' + ' is boomer')
+        boomerYes += 1
+
+async def handleBoomercount(ctx):
+    await ctx.send('yes: ' + str(boomerYes))
+    await ctx.send('no: ' + str(boomerNo))
         
 
