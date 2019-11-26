5a73a66d8ddd9a58c29ef79ec19fd54cb5674a77
jasonw52603 <minecrafter10k@gmail.com>
2019-11-25 21:00:38 -0500

    finished local test of polls

--- polls.py
+++ polls.py
@@ -7,16 +7,15 @@ def parsePoll(ctx, input):
     parsed = {'Title': title, 'Options': input}
     return parsed
 
-async def createPoll(ctx, usrInput):
+def createPoll(ctx, usrInput):
     i = 0
     #!Pass input to parse in list
     parsedInput = parsePoll(ctx, usrInput) #! This is the dict parsed
     print('Poll Title: ' + parsedInput['Title'])
-    print('')
     
     for option in parsedInput['Options']:
         i += 1
         print('Option' + str(i) + ': ' + option)
 
 
-# print(createPoll(['this is a title', 'op1', 'op2', 'op3']))
+createPoll('ctx', ['this is a title', 'op1', 'op2', 'op3'])
